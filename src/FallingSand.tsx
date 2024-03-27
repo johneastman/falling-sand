571bd63e4074a0c06f5933245eff5d5b531525f9
John Eastman <jp.robotics@gmail.com>
2024-03-27 15:49:00 -0600

    fix user interaction/how sand is dropped

--- src/FallingSand.tsx
+++ src/FallingSand.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect } from "react";
 import "./FallingSand.css";
 import Grid from "./grid";
+import { Position } from "./types";
 
 interface FallingSandProps {
     canvasWidth: number;
@@ -12,37 +13,61 @@ export default function FallingSand(props: FallingSandProps) {
     const { canvasWidth, canvasHeight, cellSize } = props;
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const isMouseDown = useRef<boolean>(false);
     const gridRef = useRef<Grid>(new Grid(canvasWidth, canvasHeight, cellSize));
+    const repeat = useRef<number>();
+    const currentMousePosition = useRef<Position>({ x: 0, y: 0 });
+
+    const mouseEventFactory = (
+        canvas: HTMLCanvasElement,
+        action: (position: Position) => void
+    ) => {
+        return (event: MouseEvent) => {
+            const rect = canvas.getBoundingClientRect();
+            const x = event.clientX - rect.left;
+            const y = event.clientY - rect.top;
+            action({ x: x, y: y });
+        };
+    };
+
+    const startSand = (position: Position) => {
+        currentMousePosition.current = position;
+        repeat.current = setInterval(() => placeCell(), 100);
+    };
+
+    const stopSand = (_: Position) => {
+        clearInterval(repeat.current);
+        repeat.current = undefined;
+    };
+
+    /**
+     * Get the current position of the mouse and place a grain of sand
+     * at that position.
+     */
+    const placeCell = () => {
+        const { x, y } = currentMousePosition.current;
+        gridRef.current.set(x, y, 1);
+    };
 
     useEffect(() => {
         const canvas = canvasRef.current!;
         const context = canvas.getContext("2d")!;
 
-        // Function to draw a circle where the user clicks
-        const placeCell = (event: MouseEvent) => {
-            event.stopImmediatePropagation();
+        const mouseDown = mouseEventFactory(canvas, startSand);
+        const mouseUp = mouseEventFactory(canvas, stopSand);
 
-            if (!isMouseDown.current) return;
-
-            const rect = canvas.getBoundingClientRect();
-            const x = event.clientX - rect.left;
-            const y = event.clientY - rect.top;
-            gridRef.current.set(x, y, 1);
-        };
-
-        const setMouseDown = (_: MouseEvent) => {
-            isMouseDown.current = true;
-        };
-
-        const setMouseUp = (_: MouseEvent) => {
-            isMouseDown.current = false;
-        };
+        /**
+         * When the mouse moves, change where sand is dropped without
+         * stopping the flow of sand.
+         */
+        const mouseMove = mouseEventFactory(
+            canvas,
+            (position: Position) => (currentMousePosition.current = position)
+        );
 
         // Add the event listener
-        canvas.addEventListener("mousemove", placeCell);
-        canvas.addEventListener("mousedown", setMouseDown);
-        canvas.addEventListener("mouseup", setMouseUp);
+        canvas.addEventListener("mousemove", mouseMove);
+        canvas.addEventListener("mousedown", mouseDown);
+        canvas.addEventListener("mouseup", mouseUp);
 
         let animationFrame: number;
         const main = () => {
@@ -53,7 +78,7 @@ export default function FallingSand(props: FallingSandProps) {
             gridRef.current.grid.forEach((color, index) => {
                 const [x, y] = gridRef.current.canvasPosition(index);
 
-                const cellColor: string = color === 0 ? "white" : "black";
+                const cellColor: string = color === 0 ? "white" : "tan";
 
                 canvasContext.beginPath();
                 canvasContext.rect(x, y, cellSize, cellSize);
@@ -69,14 +94,14 @@ export default function FallingSand(props: FallingSandProps) {
             animationFrame = requestAnimationFrame(main);
         };
 
-        // Game Loop
+        // Start game Loop
         animationFrame = requestAnimationFrame(main);
 
         // Clean up function
         return () => {
-            canvas.removeEventListener("mousemove", placeCell);
-            canvas.removeEventListener("mousedown", setMouseDown);
-            canvas.removeEventListener("mouseup", setMouseUp);
+            canvas.removeEventListener("mousemove", mouseMove);
+            canvas.removeEventListener("mousedown", mouseDown);
+            canvas.removeEventListener("mouseup", mouseUp);
 
             cancelAnimationFrame(animationFrame);
         };
